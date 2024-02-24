0724dc8f82a06d58858c02dac5fca942c4dcedc0
Mhmtshn6 <mehmet68064@gmail.com>
2024-02-24 22:59:41 +0300

    Oop basic polymorphism example
    Simple example of polymorphism, one of the basic principles of oop

---
+++ Polymorphism/Polymorphism/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polymorphism
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square s1 = new Square();
+            Circle c1 = new Circle();
+
+            s1.Draw();
+            c1.Draw();
+        }
+    }
+    class shape
+    {
+        public virtual void Draw()
+        {
+            Console.WriteLine("Draw a Shape");
+        }
+    }
+
+    class Square : shape { }
+
+    class Circle : shape
+    {
+        public override void Draw()
+        {
+            Console.WriteLine("Draw a Circle");
+        }
+    }
+
+}
